1b25fb5dfb845b36273fdf6a9c72cb3e365d3d4b
Ian Hooper <ianhooper8@gmail.com>
2020-06-30 15:24:47 -0400

    caching all files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,80 +1,41 @@
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  'app.js',
-  'favicon.ico',
   'index.js',
   'manifest.webmanifest',
-  'service-worker.js',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png',
+  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+  'service-worker.js',
   'styles.css',
-  'db.js'
+  'db.js',
 ]
 
-const CACHE_NAME = 'static-cache-v2'
+const CACHE_NAME = 'static-cache-v1'
 const DATA_CACHE_NAME = 'data-cache-v1'
 
-// install
-self.addEventListener('install', function (evt) {
+// install serviceworker
+self.addEventListener("install", function(evt) {
+  console.log('service worker installed')
   evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Your files were pre-cached successfully!')
-      return cache.addAll(FILES_TO_CACHE)
+  caches.open(CACHE_NAME).then(cache => {
+    console.log('caching files')
+cache.addAll(FILES_TO_CACHE)
+  }))
     })
-  )
-
-  self.skipWaiting()
-})
-
-// activate
-self.addEventListener('activate', function (evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log('Removing old cache data', key)
-            return caches.delete(key)
-          }
-        })
-      )
-    })
-  )
-
-  self.clients.claim()
-})
-
-// fetch
-self.addEventListener('fetch', function (evt) {
-  if (evt.request.url.includes('/api/')) {
-    evt.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone())
-              }
-
-              return response
-            })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request)
-            })
-        })
-        .catch(err => console.log(err))
-    )
-
-    return
-  }
   
+//activate service worker
+self.addEventListener('activate', evt =>{
+  console.log ('service worker activated')
+})
+
+//created fetch event when getting stuff from server
+self.addEventListener('fetch', evt =>{
+  //console.log('fetch event',evt);
   evt.respondWith(
-    caches.match(evt.request).then(function (response) {
-      return response || fetch(evt.request)
+    caches.match(evt.request).then(cacheRes =>{
+      return cacheRes || fetch(evt.request);
     })
-  )
+  );
 })
