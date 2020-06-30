8b6f75666883aec8f771fc7163a428f73eb16bf7
Ian Hooper <ianhooper8@gmail.com>
2020-06-30 17:01:17 -0400

    tweaking server.js file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  process.env.MONGODB_URI || "mongodb://heroku_f5k26xkl:Beckham8*@ds245677.mlab.com:45677/heroku_f5k26xkl", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
